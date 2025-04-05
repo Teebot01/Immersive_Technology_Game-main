37f94b5392e307da82e411bbe4ce573584e2a9a4
Teebot01 <rogersjrw1@gmail.com>
2025-04-05 14:15:01 +0100

    05/04/25
    Fixed logic for axe and planks, and added sound effects for when the axe chops through wood

--- Assets/Scripts/WoodenPlank.cs
+++ Assets/Scripts/WoodenPlank.cs
@@ -5,12 +5,37 @@ using UnityEngine;
 public class WoodenPlank : MonoBehaviour
 {
     public GameObject planksParentObject;
+    private MeshRenderer thisMeshRenderer;
+    private BoxCollider thisBoxCollider;
+    private BoxCollider thisTriggerBoxCollider = null;
+    public bool isDestroyed = false;
+    private AudioSource parentAudioSource;
+
+    private void Start()
+    {
+        thisMeshRenderer = GetComponent<MeshRenderer>();
+        thisBoxCollider = GetComponent<BoxCollider>();
+        parentAudioSource = planksParentObject.GetComponent<AudioSource>();
+
+        foreach (BoxCollider box in GetComponents<BoxCollider>())
+        {
+            if (box.isTrigger)
+            {
+                thisTriggerBoxCollider = box;
+                break; // Optional: if you only want the first one
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "AxeHead")
         {
-            Destroy(this.gameObject);
+            thisMeshRenderer.enabled = false;
+            thisBoxCollider.enabled = false;
+            thisTriggerBoxCollider.enabled = false;
+            isDestroyed = true;
+            parentAudioSource.Play();
         }
     }
 }
